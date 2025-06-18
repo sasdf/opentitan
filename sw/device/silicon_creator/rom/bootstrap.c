28c50acec10728cc45810aff14766592fbf4c87a
Yi-Hsuan Deng <yhdeng@google.com>
2025-06-18 15:42:49 +0800

    [cov] Use bank erase on bankA during ROM bootstrap
    Change-Id: I378e50c6934c6ab698bbf3a5bcf6299d2879cc1d
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/device/silicon_creator/rom/bootstrap.c
+++ sw/device/silicon_creator/rom/bootstrap.c
@@ -21,47 +21,52 @@
 extern char _instrumented_rom_slot[];
 
 rom_error_t bootstrap_chip_erase(void) {
-#ifdef OT_COVERAGE_INSTRUMENTED
-  flash_ctrl_data_default_perms_set((flash_ctrl_perms_t) {
-    .read = kMultiBitBool4False,
-    .write = kMultiBitBool4False,
-    .erase = kMultiBitBool4True,
-  });
-  for (uint32_t addr = 0; addr < (uint32_t) _instrumented_rom_slot; addr += FLASH_CTRL_PARAM_BYTES_PER_PAGE) {
-    HARDENED_RETURN_IF_ERROR(flash_ctrl_data_erase(addr, kFlashCtrlEraseTypePage));
-  }
-  flash_ctrl_data_default_perms_set((flash_ctrl_perms_t) {
-    .read = kMultiBitBool4False,
-    .write = kMultiBitBool4False,
-    .erase = kMultiBitBool4False,
-  });
-  return kErrorOk;
-
-#else
   flash_ctrl_bank_erase_perms_set(kHardenedBoolTrue);
   rom_error_t err_0 = flash_ctrl_data_erase(0, kFlashCtrlEraseTypeBank);
+#ifdef OT_COVERAGE_INSTRUMENTED
+  flash_ctrl_data_default_perms_set((flash_ctrl_perms_t){
+      .read = kMultiBitBool4False,
+      .write = kMultiBitBool4False,
+      .erase = kMultiBitBool4True,
+  });
+  for (uint32_t addr = FLASH_CTRL_PARAM_BYTES_PER_BANK;
+       addr < (uint32_t)_instrumented_rom_slot;
+       addr += FLASH_CTRL_PARAM_BYTES_PER_PAGE) {
+    HARDENED_RETURN_IF_ERROR(
+        flash_ctrl_data_erase(addr, kFlashCtrlEraseTypePage));
+  }
+  flash_ctrl_data_default_perms_set((flash_ctrl_perms_t){
+      .read = kMultiBitBool4False,
+      .write = kMultiBitBool4False,
+      .erase = kMultiBitBool4False,
+  });
+  rom_error_t err_1 = kErrorOk;
+#else
   rom_error_t err_1 = flash_ctrl_data_erase(FLASH_CTRL_PARAM_BYTES_PER_BANK,
                                             kFlashCtrlEraseTypeBank);
+#endif
   flash_ctrl_bank_erase_perms_set(kHardenedBoolFalse);
 
   HARDENED_RETURN_IF_ERROR(err_0);
   return err_1;
-#endif
 }
 
 rom_error_t bootstrap_erase_verify(void) {
-#ifdef OT_COVERAGE_INSTRUMENTED
-  for (uint32_t addr = 0; addr < (uint32_t) _instrumented_rom_slot; addr += FLASH_CTRL_PARAM_BYTES_PER_PAGE) {
-    HARDENED_RETURN_IF_ERROR(flash_ctrl_data_erase_verify(addr, kFlashCtrlEraseTypePage));
-  }
-  return kErrorOk;
-#else
   rom_error_t err_0 = flash_ctrl_data_erase_verify(0, kFlashCtrlEraseTypeBank);
+#ifdef OT_COVERAGE_INSTRUMENTED
+  for (uint32_t addr = FLASH_CTRL_PARAM_BYTES_PER_BANK;
+       addr < (uint32_t)_instrumented_rom_slot;
+       addr += FLASH_CTRL_PARAM_BYTES_PER_PAGE) {
+    HARDENED_RETURN_IF_ERROR(
+        flash_ctrl_data_erase_verify(addr, kFlashCtrlEraseTypePage));
+  }
+  rom_error_t err_1 = kErrorOk;
+#else
   rom_error_t err_1 = flash_ctrl_data_erase_verify(
       FLASH_CTRL_PARAM_BYTES_PER_BANK, kFlashCtrlEraseTypeBank);
+#endif
   HARDENED_RETURN_IF_ERROR(err_0);
   return err_1;
-#endif
 }
 
 hardened_bool_t bootstrap_requested(void) {
