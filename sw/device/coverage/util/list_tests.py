7dacdc336954e24dfc0c6871832ad98de790926f
Yi-Hsuan Deng <yhdeng@google.com>
2025-07-01 15:21:49 +0800

    [cov] Support quick target sets in test discovery tool
    Change-Id: Ib6b5a6c2f53b52741d110ad8ab2bce151c1f8590
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/device/coverage/util/list_tests.py
+++ sw/device/coverage/util/list_tests.py
@@ -42,27 +42,33 @@ def print_tests(test_list):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--extended', action='store_true',
-                      help='Include tests marked as skip_in_ci')
   args = parser.parse_args()
 
   listed = {}
   add_tests(listed, extract_tests('./run_all_coverage.sh'))
   add_tests(listed, extract_tests('./rom_targets.sh'))
 
-  useful = {}
-  add_tests(useful, extract_tests('./targets_useful_extra.sh'))
+  useful = extract_tests('./targets_useful_extra.sh')
 
-  if args.extended:
-    add_tests(listed, extract_tests('./targets_skip_in_ci.sh'))
+  extended = extract_tests('./targets_skip_in_ci.sh')
 
   minset = {}
   add_tests(minset, extract_tests('./run_min_set_coverage.sh'))
   add_tests(minset, extract_tests('./targets_min_set.sh'))
 
-  available = query_tests(args.extended)
+  quick_1m = extract_tests('./targets_quick_1m.sh')
+  quick_5m = extract_tests('./targets_quick_5m.sh')
+  quick_10m = extract_tests('./targets_quick_10m.sh')
 
-  test_groups = [available, listed, minset, useful]
+  available_ci = query_tests(False)
+  available = query_tests(True)
+
+  test_groups_names = [
+    'available', 'available_ci',
+    'listed', 'extended', 'minset', 'useful',
+    'quick_1m', 'quick_5m', 'quick_10m',
+  ]
+  test_groups = [eval(name) for name in test_groups_names]
 
   test_groups = [fix_rom_e2e_env(g) for g in test_groups]
   test_groups = [filter_tests(g, 'baseline_coverage') for g in test_groups]
@@ -70,25 +76,31 @@ def main():
   test_groups_en = [{k for k, v in g.items() if v} for g in test_groups]
   test_groups = [set(g.keys()) for g in test_groups]
 
-  available, listed, minset, useful = test_groups
-  available_en, listed_en, minset_en, useful_en = test_groups_en
+  E = dict(zip(test_groups_names, test_groups_en))
+  G = dict(zip(test_groups_names, test_groups))
 
   print()
-  print(f'Found {len(listed)} listed tests')
-  print(f'Found {len(available)} available tests')
-  print(f'Found {len(minset)} minset tests')
+  for name, group in G.items():
+    print(f'Found {len(group)} tests in {name} set')
 
   print()
   print('Minset but not listed:')
-  print_tests(minset_en - listed_en - useful_en)
-
-  print()
-  print('Listed but not available:')
-  print_tests(listed - available)
+  print_tests(E['minset'] - E['listed'] - E['extended'] - E['useful'])
 
   print()
   print('Available but not listed:')
-  print_tests(available - listed)
+  print_tests(G['available_ci'] - G['listed'])
+
+  print()
+  print('Available but not listed (extended):')
+  print_tests(G['available'] - G['available_ci'] - G['extended'])
+
+  print()
+  for name, group in G.items():
+    diff = group - G['available']
+    if len(diff):
+      print(f'Listed in {name} but not available:')
+      print_tests(diff)
 
 if __name__ == '__main__':
   main()
